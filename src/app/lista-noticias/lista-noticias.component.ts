e6a22f73236d04b532fb051305d774aebb1fa326
leonardomatumona <leonardomatumona@gmail.com>
2020-05-30 00:09:26 -0300

    atualizando

--- src/app/lista-noticias/lista-noticias.component.ts
+++ src/app/lista-noticias/lista-noticias.component.ts
@@ -13,7 +13,7 @@ export class ListaNoticiasComponent implements OnInit {
   descricao:string='Esta semana teve o lançamento do novo single do cantor Leonardo Matumona com participação da cantora argelina Yousra Boudah Africa Mama é o titulo da musica'
   image:string='https://conteudo.imguol.com.br/c/noticias/c0/2016/10/19/queda-taxa-de-juros-investimento-juros-economia-1476908627751_v2_1920x1228.jpg'
   constructor() {}
-  
+  g
 
   ngOnInit(): void {
   }
